50dd6217e9515d4d5345882a8b8a17a55672514f
Michael Perlmutter <yescrewnetwork@gmail.com>
2026-07-30 04:47:46 -0700

    Fix Twilio fallback number

--- src/app/fallback-call/route.ts
+++ src/app/fallback-call/route.ts
@@ -1,7 +1,7 @@
 const TWIML = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Dial record="record-from-answer-dual" timeout="15" action="/fallback-call">
-    <Number>+119472083261</Number>
+    <Number>+19472083261</Number>
   </Dial>
 </Response>`;
 
